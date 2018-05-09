5d9e1423dc0aa416e5341faf51577741c91062dd
errantlinguist <errantlinguist+github@gmail.com>
2018-05-08 21:16:55 -0400

    restricted access to "edu.cmu.lti.articulab.inmind.multiuser.common.model.Utils"

--- src/main/java/edu/cmu/lti/articulab/inmind/multiuser/common/model/Utils.java
+++ src/main/java/edu/cmu/lti/articulab/inmind/multiuser/common/model/Utils.java
@@ -1,3 +1,9 @@
+/*
+    Copyright (C) Carnegie Mellon University - All Rights Reserved
+    Unauthorized copying of this file, via any medium is strictly prohibited
+    This is proprietary and confidential.
+    Written by members of the ArticuLab, directed by Justine Cassell, 2018.
+ */
 package edu.cmu.lti.articulab.inmind.multiuser.common.model;
 
 import java.util.Arrays;
@@ -5,12 +11,15 @@ import java.util.Arrays;
 /**
  * Created by fpecune on 9/7/2017.
  */
-public class Utils {
+final class Utils {
 
-    public static void checkContents(String variable, String... values) throws IllegalArgumentException {
+    static void checkContents(String variable, String... values) throws IllegalArgumentException {
         if (!Arrays.asList(values).contains(variable))
             throw new IllegalArgumentException(variable);
     }
 
+    private Utils() {
+        // no instantiation
+    }
 
 }
