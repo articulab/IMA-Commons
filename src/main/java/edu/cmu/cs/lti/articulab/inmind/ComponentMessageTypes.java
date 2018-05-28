aa45de25f395f21ca638a2fffce88cdb326fcc1e
Todd Shore <tshore@cs.cmu.edu>
2018-05-28 15:08:14 -0400

    updated license headers

--- src/main/java/edu/cmu/cs/lti/articulab/inmind/ComponentMessageTypes.java
+++ src/main/java/edu/cmu/cs/lti/articulab/inmind/ComponentMessageTypes.java
@@ -1,8 +1,17 @@
 /*
-    Copyright (C) Carnegie Mellon University - All Rights Reserved
-    Unauthorized copying of this file, via any medium is strictly prohibited
-    This is proprietary and confidential.
-    Written by members of the ArticuLab, directed by Justine Cassell, 2018.
+ * Copyright 2018 Carnegie Mellon University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package edu.cmu.cs.lti.articulab.inmind;
 
@@ -108,8 +117,7 @@ public final class ComponentMessageTypes {
 	public static final String MSG_SR = "MSG_SR";
 
 	/**
-	 * A message type representing a request to start the user
-	 * interaction.
+	 * A message type representing a request to start the user interaction.
 	 */
 	public static final String MSG_START_INTERACTION = "MSG_START_INTERACTION";
 
--- src/main/java/edu/cmu/cs/lti/articulab/inmind/model/InteractionState.java
+++ src/main/java/edu/cmu/cs/lti/articulab/inmind/model/InteractionState.java
@@ -1,8 +1,17 @@
 /*
- * Copyright (C) Carnegie Mellon University - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * This is proprietary and confidential.
- * Written by members of the ArticuLab, directed by Justine Cassell, 2018.
+ * Copyright 2018 Carnegie Mellon University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package edu.cmu.cs.lti.articulab.inmind.model;
 
